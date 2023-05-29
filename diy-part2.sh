b57605f3e2aa76af448c9d1177b6f6c2994a03f6
wu1094366146 <61652427+wu1094366146@users.noreply.github.com>
2023-05-29 15:44:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 sed -i "s/OpenWrt /w1094366146 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
